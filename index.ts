25dbe6c84a9ea05b7f44be6153d4c0b96adb7c3e
Umakanth Pendyala <pendyalu@oregonstate.edu>
2025-09-25 17:40:14 -0500

    chore: add project structure and server bootstrap

---
+++ index.ts
@@ -0,0 +1,17 @@
+import { buildApp } from './app';
+
+const PORT: number = Number(process.env.PORT) || 3000;
+
+const start = async () => {
+  const app = buildApp();
+
+  try {
+    await app.listen({ port: PORT });
+    console.log(`Backend Fastify Web Server running on port: ${PORT}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
